1659c64884ab9c7da9d88bc80f3a07cfd1d92f91
luis <luisfernandogvv@gmail.com>
2022-11-15 16:21:22 -0300

    Alterando mensagem de erro

--- src/app/middlewares/server-err.ts
+++ src/app/middlewares/server-err.ts
@@ -2,7 +2,7 @@ import { Request, ErrorRequestHandler , Response, NextFunction } from "express";
 
 function serverError(error: ErrorRequestHandler, request: Request, response: Response, next: NextFunction) {
 
-    response.status(500).json({ error: 'Um erro ocorreu no servidor. Tente novamente mais tarde'});
+    response.status(500).json({ error: 'Um erro interno ocorreu. Tente novamente mais tarde'});
 
 }
 
