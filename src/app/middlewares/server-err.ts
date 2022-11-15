02a13630974a9477039e1a66f97893af4df91dce
luis <luisfernandogvv@gmail.com>
2022-11-15 19:26:13 -0300

    Refatorando middleware de erros

--- src/app/middlewares/server-err.ts
+++ src/app/middlewares/server-err.ts
@@ -2,6 +2,7 @@ import { Request, ErrorRequestHandler , Response, NextFunction } from "express";
 
 function serverError(error: ErrorRequestHandler, request: Request, response: Response, next: NextFunction) {
 
+    console.log(error);
     response.status(500).json({ error: 'Um erro interno ocorreu. Tente novamente mais tarde'});
 
 }
