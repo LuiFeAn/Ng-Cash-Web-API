f87d519fbd405e648db8241caa8f936db59355a2
luis <luisfernandogvv@gmail.com>
2022-11-14 21:14:15 -0300

    Criando arquivo de rotas, importando middleware de rotas no arquivo index

---
+++ src/app/routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const router = Router();
+
+
+router.get('/users');
+router.post('/users');
+
+
+export default router;
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import routes from './app/routes';
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(routes);
 
 app.listen(process.env.API_PORT || 3001, () => (
     console.log(` Server Started At ${process.env.API_PORT} 🔥👨‍💻`)
