edc03845037dd689f9e9b6cddeef970cdb50273d
luis <luisfernandogvv@gmail.com>
2022-11-14 21:26:21 -0300

    Importando Controllers no arquivo de rotas

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 
+import userController from "./Controllers/user-controller";
+import accountController from "./Controllers/account-controller";
+import transactionController from "./Controllers/transaction-controller";
+
 const router = Router();
 
 
-router.get('/users');
+router.get('/users',userController.show);
 router.post('/users');
 
 
