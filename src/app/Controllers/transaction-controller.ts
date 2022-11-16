5a72e5a126e95ccc57538c33c653f9c4509d1479
luis <luisfernandogvv@gmail.com>
2022-11-16 19:31:05 -0300

    Melhorando identação do código

--- src/app/Controllers/transaction-controller.ts
+++ src/app/Controllers/transaction-controller.ts
@@ -22,24 +22,38 @@ class TransactionController {
 
         const transactionErrors = [];
 
-        if(!toUser) transactionErrors.push({reason:'O usuário para o qual receberá a transação não foi específicado'});
-        if(!value) transactionErrors.push({reason:'O valor da transação não foi específicado'});
+        if(!toUser) transactionErrors.push({
+            reason:'O usuário para o qual receberá a transação não foi específicado'
+        });
 
-        if(transactionErrors.length > 0) return response.status(404).json( {error: transactionErrors} );
+        if(!value) transactionErrors.push({
+            reason:'O valor da transação não foi específicado'
+        });
+
+        if(transactionErrors.length > 0)
+        return response.status(404).json( {error: transactionErrors} );
 
         const accountRepository = AppDataSource.getRepository(Account);
         const fromUserAccount = await accountRepository.findOne({where:{ id: accountId}});
 
         if(fromUserAccount?.balance && fromUserAccount.balance < value){
-            return response.status(401).json({ error: 'Você não possui saldo o suficiente para realizar essa transação '});
+            return response.status(401).json({
+                error: 'Você não possui saldo o suficiente para realizar essa transação '}
+            );
         }
 
-        if(username === toUser) return response.status(401).json( {error: 'Você não pode realizar uma transação para si mesmo '} );
+        if(username === toUser)
+        return response.status(401).json( {
+            error: 'Você não pode realizar uma transação para si mesmo '
+        } );
 
         const userRepository = AppDataSource.getRepository(User);
 
         const toUserTransaction = await userRepository.findOne({ where: { username: toUser}});
-        if(!toUserTransaction) return response.status(404).json({ error: 'Usuário não existente !'});
+        if(!toUserTransaction)
+        return response.status(404).json({
+             error: 'Usuário não existente !'
+        });
 
         //Debitar da conta do usuário que realizou a requisição
         const debitRequestUser = fromUserAccount?.balance ? fromUserAccount.balance - value : null;
@@ -51,7 +65,11 @@ class TransactionController {
 
 
         const transactionRepository = AppDataSource.getRepository(Transaction);
-        const transaction = transactionRepository.create({debitedAccountId:fromUserAccount?.id,creditedAccountId:toUserAccount?.id, value});
+        const transaction = transactionRepository.create({
+            debitedAccountId:fromUserAccount?.id,
+            creditedAccountId:toUserAccount?.id,
+            value
+        });
         transactionRepository.save(transaction);
 
         //O usuário que realizou a transação envia o dinheiro para o usuário --tudo deu certo e o usuário ganha mais
