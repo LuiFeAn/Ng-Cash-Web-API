60233931c80049274238bee4d760fd1438a7baea
luis <luisfernandogvv@gmail.com>
2022-11-16 17:02:55 -0300

    Criando interface de Controllers e Controller de autenticação

---
+++ src/app/Controllers/auth-controller.ts
@@ -0,0 +1,2 @@
+import jwt from 'jsonwebtoken';
+import Controller from '../types/controller';
---
+++ src/app/types/controller.ts
@@ -0,0 +1,10 @@
+
+export default interface Controller {
+
+    index?: () => void,
+    show?: () => void,
+    store?: () => void,
+    delete?: () => void,
+    update?: () => void,
+
+}
