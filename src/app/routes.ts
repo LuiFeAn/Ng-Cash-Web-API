b405791711652fcedecc7e1b8efd71f9f2f1ebe1
luis <luisfernandogvv@gmail.com>
2022-11-16 17:31:11 -0300

    Criando rota de autenticação

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 
+import authController from "./controllers/auth-controller";
 import userController from "./controllers/user-controller";
 import accountController from "./controllers/account-controller";
 import transactionController from "./controllers/transaction-controller";
 
 const router = Router();
+
+router.post('/authentication',authController.authenticate);
 
 router.post('/users',userController.store);
 
