26a376c7e3e52fc1c7c4c9c468af1a3af248e196
luis <luisfernandogvv@gmail.com>
2022-12-03 19:05:16 -0300

    refact: Refatorando UserController

--- src/app/Controllers/user-controller.ts
+++ src/app/Controllers/user-controller.ts
@@ -1,11 +1,8 @@
 import { Request, Response } from "express";
-import AppDataSource from "../database";
 
+import userService from "../services/user-service";
 
-import hasUpper from "../utils/has-upper";
-import hasNumber from "../utils/has-number";
-
-import User from "../repositories/user-repository";
+import AppErr from "../errors/AppErr";
 
 class UserController  {
 
@@ -14,52 +11,21 @@ class UserController  {
 
         const { username, password } = request.body;
 
-        const repository = AppDataSource.getRepository(User);
-
-        if(!username || username.length < 3)
-        return response.status(404).json({
-            error: 'Seu nome deve possuir ao menos 3 (três) caracteres !'
-        });
-
-        const passwordErrorsFound = [];
-
-        if(!password || password.length < 8) passwordErrorsFound.push({
-            reason:'Sua senha deve possuir, no mínimo, 8 (oito) caracteres'
-        });
-
-        const verifyIfHasUpper = hasUpper(password);
-        if(!verifyIfHasUpper) passwordErrorsFound.push({
-            reason:'Sua senha deve possuir uma letra maiúscula'
-        });
-
-        const verifyIfHasNumber = hasNumber(password);
-        if(!verifyIfHasNumber) passwordErrorsFound.push({
-            reason:'Sua senha deve possuir um número'
-        });
-
-        if(passwordErrorsFound.length > 0)
-        return response.status(404).json({
-            error: passwordErrorsFound
-        });
-
-        const userExists = await repository.findOne({ where: {
-            username:username.trim()}
-        });
-
-        if(userExists)
-        return response.status(409).json({
-             error: 'Nome de usuário já existente '
-        });
-
-        const user = repository.create({
-            username: username.trim(),
+        const service = await userService.createNewUser({
+            username,
             password
         });
 
-        await repository.save(user);
+        if( service instanceof AppErr){
+            return response.status(service.statusCode).json({
+                error: service.message
+            });
+        }
 
+        response.json({
+            sucess:'Usuário cadastrado com sucesso'
+        });
 
-        response.status(200).json({ sucess: 'Usuário criado com sucesso !'});
 
     }
 
