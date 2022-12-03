62002603aea810d6f63484b25c72a8939880e7b6
luis <luisfernandogvv@gmail.com>
2022-12-03 19:03:55 -0300

    refact: Refatorando AccountController

--- src/app/Controllers/account-controller.ts
+++ src/app/Controllers/account-controller.ts
@@ -3,20 +3,19 @@ import { Request, Response } from "express";
 import { CustomRequest } from "../middlewares/jwt-verification";
 import TokenPayload from "../@types/token-payload";
 
-import Account from "../repositories/account-repository";
-import AppDataSource from "../database";
+import accountService from '../services/account-service';
+
 
 class AccountController {
 
     async session(request:Request, response: Response){
 
-        const { token } = (request as CustomRequest);
-        const { accountId } = (token as TokenPayload);
+        const { token } = request as CustomRequest;
+        const { accountId } = token as TokenPayload;
 
-        const repository = AppDataSource.getRepository(Account);
-        const userAccount = await repository.findOne({ where: { id: accountId}});
+        const service = await accountService.getAccountByTokenId(accountId);
 
-        response.json(userAccount);
+        response.json(service);
 
     }
 
