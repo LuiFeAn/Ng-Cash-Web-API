e688968d4aaecb8bc51af18f80476e03347a25e9
luis <luisfernandogvv@gmail.com>
2022-12-03 19:06:37 -0300

    refact: Refatorando Rotas

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,17 +1,28 @@
 import { Router } from "express";
 
+
+//Controllers
 import authController from "./controllers/auth-controller";
 import userController from "./controllers/user-controller";
 import accountController from "./controllers/account-controller";
 import transactionController from "./controllers/transaction-controller";
 
+//Dtos
+import userDto from "./dtos/user-dto";
+import transactionDto from "./dtos/transaction-dto";
+
+//Middlewares
 import { verifyJwt } from "./middlewares/jwt-verification";
 
 const router = Router();
 
 router.post('/authentication',authController.authenticate);
 
-router.post('/users',userController.store);
+router.post(
+    '/users',
+    userDto.post,
+    userController.store
+);
 
 router.get('/accounts',verifyJwt,accountController.session);
 
