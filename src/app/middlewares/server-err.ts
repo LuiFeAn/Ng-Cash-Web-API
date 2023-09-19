db23019ced1e1def6ba4a601d591e98ab2fb8ef6
LuiFeAn <luisfernandogvv@gmail.com>
2023-09-19 14:56:57 -0300

    refactor: Aplicando tratamaneto de exceção com base no tipo dentro do middleware global de gerenciamento de erros

--- src/app/middlewares/server-err.ts
+++ src/app/middlewares/server-err.ts
@@ -4,10 +4,22 @@ import AppErr from "../errors/AppErr";
 
 function serverError(error: AppErr, request: Request, response: Response, next: NextFunction) {
 
-   if(error){
-        return response.status( error.statusCode || 500) .json({
-            error: error.message || 'Um erro interno ocorreu. Tente novamente mais tarde'
+   if( error instanceof AppErr ){
+
+        const { errors } = error
+
+        return response.status(error.statusCode).json({
+            errors
         });
+
+   }
+
+   if( error ){
+
+        return response.status(500).json({
+            serverError:'Um erro interno ocorreu. Por favor, tente novamente mais tarde.'
+        })
+
    }
 
    next();
