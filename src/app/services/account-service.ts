d6393f0736d3e2029544bfac37aff99336c7adb4
Luis <luisfernandogvv@gmail.com>
2023-09-19 09:39:27 -0300

    feat: Aplicando regra de negócio ao método findOne
    Regra de negócio responsável por verificar se o usuário autenticado é o mesmo que está enviando o parãmetro id para obtenção da conta

--- src/app/services/account-service.ts
+++ src/app/services/account-service.ts
@@ -14,10 +14,21 @@ export class AccountService {
 
     }
 
-    async findOne(id: string){
+    async findOne(userTokenId: string,userParamId: string){
+
+        if( userTokenId != userParamId ){
+
+            throw new AppErr({
+                statusCode:401,
+                errors:[
+                    'Acesso não autorizado. Você não pode acessar a conta de outros usuários.'
+                ]
+            })
+
+        }
 
         const account = await accountRepository.findOneBy({
-            id
+            id: userParamId
         });
 
         if( !account ){
